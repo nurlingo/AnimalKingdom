662557292fb7f831a3cc0bb4be9c47fc8ef19230
nurlingo <nursultan@outlook.com>
2017-08-10 10:44:03 -0700

    Initial Commit

---
+++ Animal Kingdom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal Kingdom
+//
+//  Created by Nursultan Askarbekuly on 10/08/2017.
+//  Copyright © 2017 Nursultan Askarbekuly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
